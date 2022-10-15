e663c432c19e6cae43ae34637716ee8d8319e982
Lesley <leskimuti@gmail.com>
2022-10-15 09:53:10 +0300

    Finished array cardio challenge

--- arraycardio/main.js
+++ arraycardio/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // Get your shorts on - this is an array workout!
 // ## Array Cardio Day 1
 
@@ -81,15 +82,34 @@ console.table(ordered);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+
+console.log(totalYears);
 
 // 5. Sort the inventors by years lived
-
+const oldest = inventors.sort((a, b) =>
+  a.passed - a.year < b.passed - b.year ? 1 : -1
+);
+console.table(oldest);
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+// const category = document.querySelector(".mw-category");
+// const links = [...category.querySelectorAll("a")];
 
+// const de = links
+//   .map((link) => link.textContent)
+//   .filter((streetName) => streetName.includes("de"));
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const alpha = people.sort((a, b) => {
+  const [aLast, aFirst] = a.split(", ");
+  const [bLast, bFirst] = b.split(", ");
+  return aLast > bLast ? 1 : -1;
+});
 
+console.table(alpha);
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = [
@@ -108,3 +128,13 @@ const data = [
   "car",
   "truck",
 ];
+
+const transportation = data.reduce((obj, item) => {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+
+console.log(transportation);
