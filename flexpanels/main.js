c8d6616a1a850caa02d1c5e8c551fa88a2421639
Lesley <leskimuti@gmail.com>
2022-10-15 10:32:42 +0300

    Added the transitions on the panels

--- flexpanels/main.js
+++ flexpanels/main.js
@@ -1,1 +1,14 @@
-const panels = document.querySelectorAll(".panels");
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) =>
+  panel.addEventListener("click", function() {
+    panel.classList.toggle("open");
+  })
+);
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", function(e) {
+    if (e.propertyName.includes("flex")) {
+      panel.classList.toggle("open-active");
+    }
+  })
+);
