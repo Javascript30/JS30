79271ee5e86ff0d67ae1ac16e5a1ce7e77af281c
Lesley <leskimuti@gmail.com>
2022-10-16 23:07:03 +0300

    Update main

--- updatecssvariables/main.js
+++ updatecssvariables/main.js
@@ -1,8 +1,7 @@
 const inputs = document.querySelectorAll(".controls input");
 
-const handleUpdate = () => {
-  console.log(this);
-};
-
-inputs.forEach((input) => input.addEventListener("change", handleUpdate));
-// inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
+inputs.forEach((input) =>
+  input.addEventListener("change", function() {
+    console.log(input.value);
+  })
+);
