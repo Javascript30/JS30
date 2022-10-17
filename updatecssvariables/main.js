3f85a3fcc0aa06c834e515b41e361a5d109d439f
Leskim <leskimuti@gmail.com>
2022-10-17 05:32:06 +0300

    Shortened the solution

--- updatecssvariables/main.js
+++ updatecssvariables/main.js
@@ -1,12 +1,12 @@
 const inputs = document.querySelectorAll(".controls input");
 
+const handleUpdate = (event) => {
+  let suffix = event.target.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${event.target.name}`, event.target.value + suffix)
+}
+
 inputs.forEach((input) => {
-let suffix = input.dataset.sizing || '';
-  input.addEventListener("change", function() {
-    document.documentElement.style.setProperty(`--${input.name}`, input.value + suffix)
-  })
-  input.addEventListener("mouseover", function() {
-    document.documentElement.style.setProperty(`--${input.name}`, input.value + suffix)
-  })
+  input.addEventListener("change", handleUpdate)
+  input.addEventListener("mouseover", handleUpdate)
 });
 
